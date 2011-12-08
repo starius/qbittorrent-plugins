36909b6d40691e8d1bc034d391472ff10a3bba87
Boris Nagaev <bnagaev@gmail.com>
2011-12-08 14:00:10 +0300

    add categories support

--- tfile.py
+++ tfile.py
@@ -18,21 +18,25 @@ class tfile(object):
     url = 'http://tfile.ru';
     name = 'tfile.ru'
     supported_categories = {'all': 0,
-                            'movies': 2,
+                            'movies': 37,
                             'tv': 1068,
-                            'music': 5,
-                            'games': 18,
+                            'music': 67,
+                            'games': 98,
                             'anime': 175,
-                            'software': 9,
+                            'software': 118,
                             'pictures': 1075,
-                            'books': 10}
-    query_pattern = '%(url)s/forum/ssearch.php?q=%(q)s'
+                            'books': 195}
+    query_pattern = '%(url)s/forum/ssearch.php?f=%(f)i&q=%(q)s'
 
     def __init__(self):
         pass
 
     def search(self, what, cat='all'):
-        dat = retrieve_url(self.query_pattern % {'url': self.url, 'q': what})
+        params = {}
+        params['url'] = self.url
+        params['q'] = what
+        params['f'] = self.supported_categories[cat]
+        dat = retrieve_url(self.query_pattern % params)
         for hit in hit_pattern.finditer(dat):
             d = hit.groupdict()
             d['link'] = self.url + d['link']

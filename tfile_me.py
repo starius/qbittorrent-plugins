d23523b05e3ba38eab7a7008afeae9196e08f0f8
Dan Erusalimchik <danerde@gmail.com>
2015-04-28 22:16:34 +0300

    updates by Dan Erusalimchik

--- tfile_me.py
+++ tfile_me.py
@@ -1,5 +1,5 @@
 #VERSION: 1.00
-#AUTHORS: Boris Nagaev (bnagaev@gmail.com)
+#AUTHORS: Boris Nagaev (bnagaev@gmail.com), Dan Erusalimchik (danerde@gmail.com)
 # This plugin is licensed under the GNU GPL Version 2.
 
 from novaprinter import prettyPrinter
@@ -7,17 +7,21 @@ from helpers import retrieve_url, download_file
 import re
 from urllib import quote
 
-hit_pattern = re.compile(r'''<a href=".+">(?P<name>.+)</a>\s*
+hit_pattern = re.compile(r'''\s*<a href="(?P<desc_link>.+)">(?P<name>.+)</a>\s*
 \s*</td>\s*
 \s*<td class="dl">\s*
-\s*<b class="sd">(?P<seeds>.*)</b> \| <b class="lc">(?P<leech>.+)</b>\s*
-\s*<br/>\s*
-\s*<a href="(?P<link>.+)">(?P<size>.+)</a>\s*''')
+\s*<a href="(?P<link>.+)">(?P<size>.+)</a>\s*
+\s*</td>\s*
+\s*<td class="dl">\s*
+\s*<b class="sd">(?P<seeds>.*)</b>\s*
+\s*</td>\s*
+\s*<td class="dl">\s*
+\s*<b class="lc">(?P<leech>.+)</b>\s*''')
 tag = re.compile(r'<.*?>')
 
-class tfile(object):
-    url = 'http://tfile.ru';
-    name = 'tfile.ru'
+class tfile_me(object):
+    url = 'http://tfile.me';
+    name = 'tfile.me'
     supported_categories = {'all': 0,
                             'movies': 37,
                             'tv': 1068,
@@ -27,11 +31,9 @@ class tfile(object):
                             'software': 118,
                             'pictures': 1075,
                             'books': 195}
-    query_pattern = '%(url)s/forum/ssearch.php?f=%(f)i&q=%(q)s&start=%(start)i'
-
+    query_pattern = '%(url)s/forum/ssearch.php?q=%(q)s&c=%(f)i&start=%(start)i&o=newest&to=1&io=1'
     def __init__(self):
         pass
-
     def search_page(self, what, cat, start):
         params = {}
         params['url'] = self.url
@@ -42,10 +44,12 @@ class tfile(object):
         dat = retrieve_url(self.query_pattern % params)
         for hit in hit_pattern.finditer(dat):
             d = hit.groupdict()
+            d['desc_link'] = self.url + d['desc_link']
             d['link'] = self.url + d['link']
             d['engine_url'] = self.url
             d['name'] = tag.sub('', d['name'])
             yield d
+
 
     def search(self, what, cat='all'):
         start = 0
